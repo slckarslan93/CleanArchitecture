dbe9105e4e952312dbefc40b51ac1eb2bf95bc7f
Selcuk <slckarslan93@gmail.com>
2025-04-08 21:43:39 +0300

    Add Scalar API support and new presentation components
    - Updated `CleanArchitecture.Presentation.csproj` to include `Microsoft.AspNetCore.Mvc.Core` package.
    - Modified `CleanArchitecture.WebApi.csproj` to add `Scalar.AspNetCore` package and project reference.
    - Enhanced `Program.cs` to register Scalar API and presentation layer controllers.
    - Updated `launchSettings.json` to enable browser launch and set launch URL.
    - Added `AssemblyReference.cs` for assembly reference management.
    - Created `ValuesController.cs` with a simple GET endpoint.

---
+++ CleanArchitecture.Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Deneme ap'islemi basarili");
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,17 +1,19 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly); //controller'larýn bulunduðu assembly'i ekliyoruz (artik presenrarion katmaninda olacak controller lar)
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
