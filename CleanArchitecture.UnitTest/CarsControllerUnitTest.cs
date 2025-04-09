a1ce115a284526b8f92228b2ab78fa8ac6f0b80d
Selcuk <slckarslan93@gmail.com>
2025-04-09 15:40:35 +0300

    Add unit tests for CarsController and update project file
    Updated `CleanArchitecture.UnitTest.csproj` to include
    new package references for `MediatR` and `Moq`, and added
    project references to `CleanArchitecture.Application` and
    `CleanArchitecture.Presentation`. Removed redundant
    `UnitTest1.cs` file. Introduced `CarsControllerUnitTest`
    with a test for the `Create` method, enhancing test
    coverage for the controller functionality.

---
+++ CleanArchitecture.UnitTest/CarsControllerUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using CleanArchitecture.Application.Features.CarFeatures.Commmands.CreateCar;
+using CleanArchitecture.Domain.Dtos;
+using CleanArchitecture.Presentation.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CleanArchitecture.UnitTest
+{
+    public class CarsControllerUnitTest
+    {
+        [Fact]
+        public async Task Create_ReturnsOkResult_WhenRequestIsValid()
+        {
+            //arrange
+            var mediatrMock = new Mock<IMediator>();
+            CreateCarCommand createCarCommand = new CreateCarCommand("Toyota","Corolla",5000);
+            MessageResponse response = new("Car created successfully");
+            CancellationToken cancellationToken = new();
+
+            mediatrMock.Setup(m=> m.Send(createCarCommand,cancellationToken)).ReturnsAsync(response);
+            CarsController carsController = new(mediatrMock.Object);
+
+            //act
+            var result = await carsController.Create(createCarCommand, cancellationToken);
+
+            //assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<MessageResponse>(okResult.Value);
+
+            Assert.Equal(response, returnValue);
+            mediatrMock.Verify(m => m.Send(createCarCommand, cancellationToken), Times.Once);
+        }
+    }
+}
