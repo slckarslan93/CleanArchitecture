f47e80b6380d469600158d072208e30fdb9b2634
Selcuk <slckarslan93@gmail.com>
2025-04-10 11:44:32 +0300

    Add IMailService interface and project reference
    Updated `CleanArchitecture.Infrastructure.csproj` to include a reference to `CleanArchitecture.Application.csproj`. Added `IMailService` interface for asynchronous email sending. Commented out the implementation in `MailServices.cs`, indicating that the developer should provide the actual service logic.

---
+++ CleanArchitecture.Infrastructure/Services/MailServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Services;
+
+namespace CleanArchitecture.Infrastructure.Services;
+//public sealed class MailServices : IMailService
+//{
+//    public Task SendMailAsync()
+//    {
+        
+//    }
+//}
+
+// mail serviceyi kendin yaz
