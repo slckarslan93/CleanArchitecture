10479b8c9fe79d810ac75a91ad57aa10599d57cb
Selcuk <slckarslan93@gmail.com>
2025-04-14 12:15:08 +0300

    Refactor user role handling and add authorization filter
    Removed `CreateUserRole.cs` and added `CreateUserRoleCommand.cs` for better organization. Introduced a new `RoleAttribute` class to handle authorization based on user roles, improving security checks in the application.

---
+++ CleanArchitecture.Infrastructure/Authorization/RoleAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CleanArchitecture.Infrastructure.Authorization;
+public sealed class RoleAttribute : Attribute, IAuthorizationFilter
+{
+    private readonly string _role;
+    private readonly IUserRoleRepository _userRoleRepository;
+
+    public RoleAttribute(string role, IUserRoleRepository userRoleRepository)
+    {
+        _role = role;
+        _userRoleRepository = userRoleRepository;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var userIdClaim = context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+        //var userId = Guid.Parse(userIdClaim.Value);
+        //var hasRole = _userRoleRepository.HasRoleAsync(userId, _role).Result; //kendi generic reponu yazinca eklersin methodu 
+        //if (!hasRole)
+        //{
+        //    context.Result = new ForbidResult();
+        }
+    }
+}
