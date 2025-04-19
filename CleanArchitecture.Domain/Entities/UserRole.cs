c142eb6700425e690794dc29e271223defa78727
Selcuk ARSLAN <slckarslan93@gmail.com>
2025-04-19 17:44:13 +0300

    updated

--- CleanArchitecture.Domain/Entities/UserRole.cs
+++ CleanArchitecture.Domain/Entities/UserRole.cs
@@ -2,13 +2,16 @@
 using CleanArchitecture.Domain.Abstractions;
 
 namespace CleanArchitecture.Domain.Entities;
+
 public sealed class UserRole : Entity
 {
     [ForeignKey("User")]
     public string UserId { get; set; }
+
     public User User { get; set; }
 
     [ForeignKey("Role")]
     public string RoleId { get; set; }
+
     public Role Role { get; set; }
 }
--- CleanArchitecture.Presentation/Controllers/CarsController.cs
+++ CleanArchitecture.Presentation/Controllers/CarsController.cs
@@ -8,11 +8,13 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.Presentation.Controllers;
+
 public sealed class CarsController : ApiController
 {
     public CarsController(IMediator mediator) : base(mediator)
     {
     }
+
     [RoleFilter("Create")]
     [HttpPost("[action]")]
     public async Task<IActionResult> Create(CreateCarCommand request, CancellationToken cancellationToken)
@@ -20,6 +22,7 @@ public sealed class CarsController : ApiController
         MessageResponse response = await _mediator.Send(request, cancellationToken);
         return Ok(response);
     }
+
     [RoleFilter("GetAll")]
     [HttpPost("[action]")]
     public async Task<IActionResult> GetAll(GetAllCarQuery request, CancellationToken cancellationToken)
